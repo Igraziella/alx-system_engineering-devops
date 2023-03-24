461ad209b7d9cc429cd5358ffeb80081e592abf3
igraziella <igraziella2@gmail.com>
2023-03-24 09:03:31 -0700

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Installing flask version 2.1.0 on a puppet agent.
-package { 'flask:'
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
