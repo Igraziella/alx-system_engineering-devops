d6240fed7a574eb4dbad5eec8247e0561f6a7c7b
igraziella <igraziella2@gmail.com>
2023-03-28 04:47:29 -0700

    completed all tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# A puppet manifesto to create a file in /tmp
+# A puppet manifest to create a file in /tmp
 file { '/tmp/school':
   ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => "I love Puppet\n",
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Creates a manifest that kills the process named killmenow
-exec { 'kills a process named killmenow':
-  command  => 'pkill killnenow',
-  path     => '/bin:/usr/bin',
-  provider => 'shell',
+exec { 'killmenow':
+  command => 'pkill killmenow',
+  path    => '/bin:/usr/bin',
+  onlyif  => 'pgrep killmenow',
 }
