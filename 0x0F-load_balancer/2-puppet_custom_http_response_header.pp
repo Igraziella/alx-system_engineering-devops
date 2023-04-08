e28a2af0e14ced8c54140e979ed4cef94e7e5ac2
Obialor Ijeoma G <109988413+Igraziella@users.noreply.github.com>
2023-04-08 13:27:28 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,5 @@
 # automate the task of creating a custom HTTP header response, using Puppet.
-<<<<<<< HEAD
-exec { 'command'
-=======
 exec { 'command':
->>>>>>> 69d8ab3d77787f2194ef82804c1dda07dfe13c4a
   command  => 'apt-get -y update;
   apt-get -y install nginx;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
