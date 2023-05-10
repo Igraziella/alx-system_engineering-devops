541b89d21c57658a0931137440fd190b9b0e1259
igraziella <igraziella2@gmail.com>
2023-05-10 13:26:12 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Queries the Reddit API """
+"""  a function that queries the Reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ Returns the total number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'alx-system_engineering-devops/\
-            1.0/0x16-api_advanced'}
+    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
+    headers = {"User-Agent": "0x16-api_advanced:project: v1.0.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
