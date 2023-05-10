e2725ee18bad97e7b6b3e3ecd47e4742e0758bea
igraziella <igraziella2@gmail.com>
2023-05-10 12:03:10 -0700

    minor changes made

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API
-"""
+""" Queries the Reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns the total number of subscribers
-        for a given subreddit.
-    """
+    """ Returns the total number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'alx-system_engineering-devops/\
             1.0/0x16-api_advanced'}
