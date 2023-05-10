a078b484961a33db47c041e9e994e7944777343a
igraziella <igraziella2@gmail.com>
2023-05-10 09:28:57 -0700

    modified task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,18 +9,23 @@ def top_ten(subreddit):
     """ Prints the titles of the first 10 hot posts listed
         for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'alx-system_engineering-devops/\
-            1.0/0x16-api_advanced'}
+    url = f"https://www.reddit.com/r/{subreddit}/top/.json"
+    headers = {'User-Agent': 'alx-system engineering-devops/1.0/0x16-api'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {
+        "limit": 10,
+    }
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
+    response = requests.get(
+            url=url, headers=headers,
+            allow_redirects=False,
+            params=params)
 
-        for i, post in enumerate(posts[:10], 1):
-            title = post['data']['title']
-            print(f"{i}. {title}")
-        else:
-            print("None")
+    data = response.json()
+
+    if "data" in data and "children" in data["data"]:
+        # Iterate through the top posts, print and post number and title
+        for i, post in enumerate(data["data"]["children"], start=1):
+            print(f"{i}: {post['data']['title']}")
+    else:
+        print("None")
