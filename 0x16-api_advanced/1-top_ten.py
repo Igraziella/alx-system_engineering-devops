41e8f40ff4e479e1d2c76e839022927c538fc8b9
igraziella <igraziella2@gmail.com>
2023-05-10 11:47:11 -0700

    added task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,17 +6,15 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /lou and ijay)"
-    }
+    headers = {"User-Agent": "0x16-api_advanced:project: v1.0.0"}
+
     params = {
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    results = response.json().get("data").get("children")
+    [print(c.get("data").get("title")) for c in results]
