64d5b6fa400eb6a384e1888a2026af54bd4f667d
igraziella <igraziella2@gmail.com>
2023-05-14 09:12:53 -0700

    made changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     user_todos = [todo for todo in todos if todo.get("userId") == userId]
 
     # Count the number of completed tasks
-    completed_todos = [todo for todo in user_todos if todo.get("completed")]
+    completed_todos = [todo for todo in user_todos if todo.get("completed") == True]
 
     # Display the progress
     print("Employee {} is done with tasks({}/{}):".format(
                         name, len(completed_todos), len(user_todos)))
     for todo in todos:
-        if todo['completed']:
-            print("\t {}".format(todo['title']))
+        if todo["completed"]:
+            print("\t {}".format(todo["title"]))
