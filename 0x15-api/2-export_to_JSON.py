e65c418a44d9aad70862a968c211bc5d72ea7f24
igraziella <igraziella2@gmail.com>
2023-05-15 03:59:33 -0700

    modified task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,6 +3,7 @@
     in the JSON format.
 """
 import json
+import re
 import requests
 import sys
 
@@ -11,22 +12,25 @@ if __name__ == '__main__':
     """ Get the todo list progress of an employee """
     API = "https://jsonplaceholder.typicode.com"
 
-    user_id = int(sys.argv[1])
-    user_endpoint = "{}/users/{}".format(API, user_id)
-    username = requests.get(user_endpoint).json().get("username")
-    tasks_endpoint = "{}/todos".format(API)
-    tasks = requests.get(tasks_endpoint).json()
-    user_todos = {
-        user_id: [
-            {
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": username
-            }
-            for task in tasks if task.get("userId") == user_id
-        ]
-    }
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            user_id = int(sys.argv[1])
+            user_endpoint = "{}/users/{}".format(API, user_id)
+            username = requests.get(user_endpoint).json().get("username")
+            tasks_endpoint = "{}/todos".format(API)
+            tasks = requests.get(tasks_endpoint).json()
+            user_todos = {
+                    user_id: [
+                        {
+                            "task": task.get("title"),
+                            "completed": task.get("completed"),
+                            "username": username
+                        }
+                        for task in tasks if task.get("userId") == user_id
+                        ]
+                    }
 
-    # save in json file
-    with open("{}.json".format(user_id), 'w', encoding="utf-8") as file:
-        json.dump(user_todos, file)
+            # save in json file
+            with open("{}.json".format(user_id), 'w', encoding="utf-8")
+            as file:
+                json.dump(user_todos, file)
