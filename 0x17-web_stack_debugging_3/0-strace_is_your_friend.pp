00bd383c44865bc60c0d4deb2435a15db29ba947
igraziella <igraziella2@gmail.com>
2023-05-18 12:46:38 -0700

    added comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# script to automate the debugging of Apache
+
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
